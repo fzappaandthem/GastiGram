1619f6ee10deb2f9e188c4df3cdcd0eda974754e
Gastón <fzappaandthem@gmail.com>
2020-09-08 02:28:52 -0300

    agrego login y register

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    gastiGram
                 </div>
 
                 <div class="links">
